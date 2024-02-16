82e5ffcee710ec505ce05ffba72faec6a6ab6769
jerrybowang <jerrybowang@icloud.com>
2024-02-16 12:40:01 -0800

    change main logic to use result_generator

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 import host_discovery as hd
 import port_scanning as ps
 import directory_enumeration as de
+import result_generator as rg
 
 if __name__ == "__main__":
     # initialize parser
@@ -42,16 +43,26 @@ if __name__ == "__main__":
         if len(args.enumeration) > 2:
             parser.error("Invalid number of arguments for Directory Enumeration module, at most 2 arguments are allowed")
 
+    # result variables
+    host_discovery_result = None
+    port_scanning_result = None
+    directory_enumeration_result = None
     # main logic
     if args.discovery:
         print(f"Host Discovery module with argument: {args.discovery[0]}")
         # run host discovery module
-        result = hd.host_discovery(args.discovery[0])
-        print(result)
-    elif args.scanning:
+        host_discovery_result = hd.host_discovery(args.discovery[0])
+        print(host_discovery_result)
+    if args.scanning:
         print(f"Port Scanning module with argument: {args.scanning[0]}")
-    elif args.enumeration:
+    if args.enumeration:
         print(f"Directory Enumeration module with argument: {args.enumeration[0]} and wordlist: {args.enumeration[1] if len(args.enumeration) > 1 else 'default'}")
-    else:
+    if not any(vars(args).values()):
         # display help message when no argument is passed
         parser.print_help()
+        parser.exit()
+    
+    # result generation
+    
+    
+
